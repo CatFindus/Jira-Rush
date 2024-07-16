6a4447da6ec690d712277896722c9bb84ad9ec61
Anton Puchinets <apuchinec@gmail.com>
2024-07-16 19:09:01 +0300

    Task 4:Переделать тесты так, чтоб во время тестов использовалась in memory БД (H2), а не PostgreSQL. Для этого нужно определить 2 бина, и выборка какой из них использовать должно определяться активным профилем Spring. H2 не поддерживает все фичи, которые есть у PostgreSQL, поэтому тебе прийдется немного упростить скрипты с тестовыми данными. Тестирование переделано на in memory DB H2. Отредактирован application-test.yaml, подключена база H2. Создан changelog-test.sql с откорректированными для H2 скриптами. Отредактирован data.sql для возможности использования с DB H2. Отредактирован класс AbstractControllerTest для использования с новым файлом changelog-test.sql.

--- src/main/java/com/javarush/jira/JiraRushApplication.java
+++ src/main/java/com/javarush/jira/JiraRushApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
@@ -14,6 +15,6 @@ import org.springframework.context.annotation.Profile;
 public class JiraRushApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JiraRushApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(JiraRushApplication.class, args);
     }
 }
